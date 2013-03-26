7a5602079123b36633dc8e88ab8f2fa50b808183
William O'Hanley <willy.ohanley@gmail.com>
2013-03-26 00:02:40 -0300

    Initial commit

---
+++ wiki-mapper.py
@@ -0,0 +1,66 @@
+import argparse
+import urllib2
+from bs4 import BeautifulSoup
+import re
+import os
+import sys
+from googlemaps import GoogleMaps
+from googlemaps import GoogleMapsError
+from pykml.factory import KML_ElementMaker as kml
+from lxml import etree
+import unicodedata
+
+parser = argparse.ArgumentParser(description="Generates a KML map file from a Wikipedia list article.")
+parser.add_argument('source_url')
+parser.add_argument('gm_api_key')
+parser.add_argument('-o', '--out', default='map.kml')
+args = parser.parse_args()
+
+# Ssshhhh, don't tell Wikipedia
+req = urllib2.Request(args.source_url, headers={'User-Agent' : "Magic Browser"})
+html = BeautifulSoup(urllib2.urlopen(req))
+
+locations = []
+
+for item in html.select('h3 span.mw-headline'):
+    header = item.parent
+    text = ''.join(header.next_sibling.next_sibling.find_all(text=True))
+    text = re.sub(r'\[.*\]', '', text)
+    text = os.linesep.join([s for s in text.splitlines() if s])
+    print(header)
+    locations.append((header.contents[2].string, text))
+
+maps = GoogleMaps(args.gm_api_key)
+
+doc = kml.kml(
+    kml.Document(
+        kml.name("Berlin Wall Segments"),
+        kml.Style(
+            kml.IconStyle(
+                kml.Icon(
+                    kml.href("http://maps.google.com/mapfiles/kml/paddle/wht-circle.png")
+                )
+            )
+        )
+    )
+)
+
+for location in locations:
+    try:
+        coordinates = maps.address_to_latlng(unicodedata.normalize('NFKD', location[0]).encode('ascii','ignore'))
+        doc.Document.append(kml.Placemark(
+            kml.name(location[0]),
+            kml.description(location[1]),
+            kml.Point(
+                kml.coordinates(','.join(format(x, "3.6f") for x in [coordinates[1], coordinates[0], 0]))
+            )
+        ))
+    except GoogleMapsError as gme:
+        sys.stderr.write(str(gme.message) + " on " + str(location[0].encode('ascii', 'replace')) + '\n')
+    except TypeError as te:
+        if location[0]:
+            sys.stderr.write(str(te.message) + " on " + str(location[0].encode('ascii', 'replace')) + '\n')
+
+out = open(args.out, 'w')
+out.write("""<?xml version="1.0" encoding="UTF-8"?>""")
+out.write(etree.tostring(doc, pretty_print=True))
